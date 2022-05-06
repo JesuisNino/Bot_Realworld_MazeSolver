2a35a1da2ac1971cbd13f5683685a4edd8db0f95
Xiaofeng Hu <xhu73@sheffield.ac.uk>
2022-05-06 09:06:32 +0100

    deleted old ones and created new ones for task 3

--- src/maze_server.py
+++ src/maze_server.py
@@ -1,39 +1,104 @@
-#! /usr/bin/env python3
-import imp
-import rospy # Python library for ROS
+#! /usr/bin/python3
 
-from maze_controller import Controller
-from sensor_msgs.msg import LaserScan # LaserScan type message is defined in sensor_msgs
-from geometry_msgs.msg import Twist #
+# Import the core Python modules for ROS and to implement ROS Actions:
+import rospy
+import actionlib
 
-def callback(tb):
-    tb = Controller()
-    turn = False
+# Import all the necessary ROS message types:
+from com2009_msgs.msg import SearchFeedback, SearchResult, SearchAction, SearchGoal
 
-    thr1 = 0.8 # Laser scan range threshold
-    thr2 = 0.8
-    if tb.ranges[0]>thr1 and tb.ranges[15]>thr2 and tb.ranges[345]>thr2: # Checks if there are obstacles in front and
-                                                                         # 15 degrees left and right (Try changing the
-									 # the angle values as well as the thresholds)
-        tb.moveForward(0.5)
-        turn = False
-    else:
-        tb.turnLeft(90)
-        turn = True
-        if tb.ranges[0]>thr1 and tb.ranges[15]>thr2 and tb.ranges[345]>thr2:
-            tb.moveForward(0.5)
-            turn = False
-    pub.publish(tb) # publish the object
-    # Get the position of the robot from the onboard sensors
+# Import the tb3 modules from tb3.py
+from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
 
-vel = Twist() # Creates a Twist message type object
-rospy.init_node('maze_nav') # Initializes a node
-pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)  # Publisher object which will publish "Twist" type messages
-                            				 # on the "/cmd_vel" Topic, "queue_size" is the size of the
-                                                         # outgoing message queue used for asynchronous publishing
+# Import some other useful Python Modules
+from math import sqrt, pow
+import numpy as np
 
-sub = rospy.Subscriber("/scan", LaserScan, callback)  # Subscriber object which will listen "LaserScan" type messages
-                                                      # from the "/scan" Topic and call the "callback" function
-						      # each time it reads something from the Topic
+class SearchActionServer(object):
+    feedback = SearchFeedback() 
+    result = SearchResult()
 
-rospy.spin() # Loops infinitely until someone stops the program execution
+    def __init__(self):
+        self.actionserver = actionlib.SimpleActionServer("/obstacle_action_server", 
+            SearchAction, self.action_server_launcher, auto_start=False)
+        self.actionserver.start()
+
+        self.vel_controller = Tb3Move()
+        self.tb3_odom = Tb3Odometry()
+        self.tb3_lidar = Tb3LaserScan()
+    
+    def scan_callback(self, scan_data):
+        left_arc = scan_data.ranges[0:21]
+        right_arc = scan_data.ranges[-20:]
+        front_arc = np.array(left_arc[::-1] + right_arc[::-1])
+        self.min_distance = front_arc.min()
+        self.object_angle = self.arc_angles[np.argmin(front_arc)]
+    
+    def action_server_launcher(self, goal: SearchGoal):
+        r = rospy.Rate(10)
+
+        success = True
+        if goal.fwd_velocity <= 0 or goal.fwd_velocity > 0.26:
+            print("Invalid velocity.  Select a value between 0 and 0.26 m/s.")
+            success = False
+        if goal.approach_distance <= 0.2:
+            print("Invalid approach distance: I'll crash!")
+            success = False
+        elif goal.approach_distance > 3.5:
+            print("Invalid approach distance: I can't measure that far.")
+            success = False
+
+        if not success:
+            self.actionserver.set_aborted()
+            return
+
+        print(f"Request to move at {goal.fwd_velocity:.3f}m/s "
+                f"and stop {goal.approach_distance:.2f}m "
+                f"infront of any obstacles")
+
+        # Get the current robot odometry:
+        self.posx0 = self.tb3_odom.posx
+        self.posy0 = self.tb3_odom.posy
+        
+        print("The robot will start to move now...")
+        # set the robot velocity:
+        self.vel_controller.set_move_cmd(goal.fwd_velocity, 0.0)
+        
+        while self.tb3_lidar.min_distance > goal.approach_distance:
+            self.vel_controller.publish()
+            # check if there has been a request to cancel the action mid-way through:
+            if self.actionserver.is_preempt_requested():
+                self.actionserver.set_preempted()
+                # stop the robot:
+                self.vel_controller.stop()
+                success = False
+                # exit the loop:
+                break
+            
+            self.distance = sqrt(pow(self.posx0 - self.tb3_odom.posx, 2) + pow(self.posy0 - self.tb3_odom.posy, 2))
+            # populate the feedback message and publish it:
+            self.feedback.current_distance_travelled = self.distance
+            self.actionserver.publish_feedback(self.feedback)
+
+        # turn away when meet a collision
+        print("Collision found, turning away...")
+        if self.tb3_lidar.closest_object_position > 0:
+            self.vel_controller.set_move_cmd(0, 1.8)
+        else:
+            self.vel_controller.set_move_cmd(0, -1.8)
+
+        self.vel_controller.stop()
+
+        if success:
+            rospy.loginfo("approach completed sucessfully.")
+            self.result.total_distance_travelled = self.distance
+            self.result.closest_object_distance = self.tb3_lidar.min_distance
+            self.result.closest_object_angle = self.tb3_lidar.closest_object_position
+
+            self.actionserver.set_succeeded(self.result)
+            self.vel_controller.stop()
+            
+if __name__ == '__main__':
+    rospy.init_node("obstacle_action_server")
+    SearchActionServer()
+    rospy.spin()
